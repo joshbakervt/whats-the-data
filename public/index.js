57852e2ff382bba2626aeb391a058f31ac1d336f
joshbakervt <joshbakervt@gmail.com>
2022-09-27 18:01:57 -0400

    Adjusting display

--- public/index.js
+++ public/index.js
@@ -10,6 +10,75 @@ function getResponse() {
         });
 }
 
+// function getCategories(data, subject) {
+//     var uniqueCategories = [];
+//     var table = "<table>";
+//
+//     for (let i = 0; i < data.length; i++) {
+//         if (data[i].subject == subject) {
+//             if (uniqueCategories.includes(data[i].region, 0) == false){
+//                 uniqueCategories.push(data[i].region);
+//             }
+//             // subjectCategories.push(data[i].region);
+//         }
+//     }
+//
+//     for (let i = 0; i < uniqueCategories.length; i++) {
+//         var row = "<td><strong><a href=" + uniqueCategories[i] + " target=\"_blank\">" + uniqueCategories[i] + "</strong></td>";
+//         table += row + "</tr>";
+//     }
+//
+//     return(table + "</table>");
+// }
+//
+// function displayData(data, subject) {
+//     var subjectData = [];
+//     data.sort();
+//     for (let i = 0; i < data.length; i++) {
+//         if (data[i].subject == subject) {
+//             subjectData.push(data[i]);
+//         }
+//     }
+//
+//     var table = "<table>";
+//     table += "<tr><td></td><td></td><td>Description</td><td>Study Type</td><td>Size</td><td>Author</td><td>Reference</td></tr>"
+//
+//     for (let i = 0; i < subjectData.length; i++) {
+//         var row = "<td style='text-decoration: underline'><strong><a href=" + subjectData[i].link + " target=\"_blank\">Summary</strong></td>";
+//         row += "<td style='text-decoration: underline'><strong><a href=" + subjectData[i].link + " target=\"_blank\">Pubmed</strong></td>";
+//         row += "<td>" + subjectData[i].description + "</td>";
+//         row += "<td>" + subjectData[i].study_type + "</td>";
+//         row += "<td>" + subjectData[i].size + "</td>";
+//         row += "<td>" + subjectData[i].author + "</td>";
+//         row += "<td>" + subjectData[i].reference + "</td>";
+//         // row += "<td>" + subjectData[i].summary + "</td>";
+//         // row += "<td>" + subjectData[i].inclusion + "</td>";
+//         // row += "<td>" + subjectData[i].exclusion + "</td>";
+//         // row += "<td>" + subjectData[i].design_intervention + "</td>";
+//         // row += "<td>" + subjectData[i].follow_up + "</td>";
+//         // row += "<td>" + subjectData[i].results + "</td>";
+//         // row += "<td>" + subjectData[i].notes + "</td>";
+//         table += row + "</tr>";
+//     }
+//     return(table + "</table>");
+//     // cD.innerHTML = JSON.stringify(cerebrovascularData[0]);
+// }
+
+function getCategoriesAsText(data, subject) {
+    var categoriesText = [];
+
+    for (let i = 0; i < data.length; i++) {
+        if (data[i].subject == subject) {
+            if (categoriesText.includes(data[i].region, 0) == false){
+                categoriesText.push(data[i].region);
+            }
+            // subjectCategories.push(data[i].region);
+        }
+    }
+
+    return categoriesText;
+}
+
 function getCategories(data, subject) {
     var uniqueCategories = [];
     var table = "<table>";
@@ -31,8 +100,9 @@ function getCategories(data, subject) {
     return(table + "</table>");
 }
 
-function displayData(data, subject) {
+function displayData(data, subject, categories) {
     var subjectData = [];
+    data.sort();
     for (let i = 0; i < data.length; i++) {
         if (data[i].subject == subject) {
             subjectData.push(data[i]);
@@ -42,6 +112,12 @@ function displayData(data, subject) {
     var table = "<table>";
     table += "<tr><td></td><td></td><td>Description</td><td>Study Type</td><td>Size</td><td>Author</td><td>Reference</td></tr>"
 
+    // TODO: Create specific table for each region
+    // for (let j = 0; j < categories.length; j++) {
+    //     var row = "<td>" + categories[j] + "</td>";
+    //     table += row + "</tr>";
+    // }
+    table += categories;
     for (let i = 0; i < subjectData.length; i++) {
         var row = "<td style='text-decoration: underline'><strong><a href=" + subjectData[i].link + " target=\"_blank\">Summary</strong></td>";
         row += "<td style='text-decoration: underline'><strong><a href=" + subjectData[i].link + " target=\"_blank\">Pubmed</strong></td>";
